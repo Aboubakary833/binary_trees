83d91b0ad693e2b045ebeb41856b42f337b6b242
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-08 21:48:43 +0000

    chores: 0-binary_tree_print

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Create a binary tree node
+ * @parent: parent node pointer
+ * @value: value for new node
+ *
+ * Return: The actual binary tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *bt = malloc(sizeof(binary_tree_t));
+
+	if (!bt)
+		return (NULL);
+
+	bt->n = value;
+	bt->parent = parent;
+
+	return (bt);
+}
