d7efb40d2667ebf8fa18565ec8b7a60b1a17da55
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-09 20:39:15 +0000

    chores: 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 	node = binary_tree_node(parent, value);
-	if (!node)
-		return (NULL);
+	if (parent->left)
+	{
+		binary_tree_t *left = parent->left;
+
+		node->left = left;
+	}
 	parent->left = node;
 
 	return (node);
