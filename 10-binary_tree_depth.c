3131176c3c14154f50376cf566714a6d7c78b5cb
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-11 23:24:26 +0000

    chores: Rapid change from my android termux

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree - The tree root
+ * Return: depth of the tree or 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree) {
+	size_t depth = 0;
+	
+	if (!tree)
+		return (0);
+	depth += binary_tree_depth(tree->parent);
+
+	return (depth);
+}
