64d38cc0314b58fc61a29b7518b8219795492762
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-11 23:58:30 +0000

    chores: Rapid change from my android termux

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measure the size of a tree
+ * @tree: The tree
+ * Return: The size or 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    size_t size;
+
+    if (!tree)
+        return (0);
+    size = 1 + binary_tree_size(tree->left);
+    size += binary_tree_size(tree->right);
+
+    return (size);
+}
