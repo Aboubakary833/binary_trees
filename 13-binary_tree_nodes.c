b709d0cbd92bb8004cf3078eed7a7138fcd0cee8
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-12 14:43:54 +0000

    chores: 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,38 +1,21 @@
 #include "binary_trees.h"
 
-size_t get_binary_tree_height(const binary_tree_t *tree);
-
 /**
  * binary_tree_nodes - Count the nodes with at least 1 child
  * @tree: Tree root
- * To count binary tree node, use the formula: (2h - 1)
- * where h equals to the height of the binary tree
  * Return: Number of node's children or 0
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t nodeCount = 0;
 
 	if (!tree)
 		return (0);
-	height = get_binary_tree_height(tree);
+	if (tree->left || tree->right)
+		nodeCount++;
 
-	return ((height * 2) - 1);
-}
+	nodeCount += binary_tree_nodes(tree->left);
+	nodeCount += binary_tree_nodes(tree->right);
 
-/**
- * get_binary_tree_height - Mesure hight of a tree
- * @tree: The tree
- * Return: The height of a tree or 0
- */
-size_t get_binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left, right;
-
-	if (!tree)
-		return (0);
-	left = tree->left ? 1 + get_binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + get_binary_tree_height(tree->right) : 0;
-
-	return (left > right ? left : right);
+	return (nodeCount);
 }
