9f681fe619cbabc6a80a2eab997eeb62e35bd88c
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-12 16:51:10 +0000

    chores: 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,6 +13,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 	leftHeight = get_binary_tree_height(tree->left);
 	rightHeight = get_binary_tree_height(tree->right);
 
