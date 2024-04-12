a860dd8a7b8555a72aa085fb5230de4452867dc1
Aboubakary833 <aboubakarycisse472@gmail.com>
2024-04-12 20:52:34 +0000

    chores: 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-int get_binary_tree_height(const binary_tree_t *tree);
+int get_binary_tree_depth(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_perfect - Check if a binary tree is perfect
@@ -15,8 +15,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	leftHeight = get_binary_tree_height(tree->left);
-	rightHeight = get_binary_tree_height(tree->right);
+	leftHeight = get_binary_tree_depth(tree->left);
+	rightHeight = get_binary_tree_depth(tree->right);
 
 	if (!(leftHeight - rightHeight))
 		return (1);
@@ -24,18 +24,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 }
 
 /**
- * get_binary_tree_height - Mesure hight of a tree
- * @tree: The tree
- * Return: The height of a tree or 0
+ * get_binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: The tree root
+ * Return: depth of the tree or 0
  */
-int get_binary_tree_height(const binary_tree_t *tree)
+int get_binary_tree_depth(const binary_tree_t *tree)
 {
 	int left, right;
 
 	if (!tree)
 		return (0);
-	left = tree->left ? 1 + get_binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + get_binary_tree_height(tree->right) : 0;
+	left = get_binary_tree_depth(tree->left);
+	right = get_binary_tree_depth(tree->right);
 
-	return (left > right ? left : right);
+	return (left + 1 + right);
 }
